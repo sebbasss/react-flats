417e1e8c3bc062c7b184a24f5a141e587a102dce
Sebastian Jara <s.jaranilsson@gmail.com>
2022-06-27 19:01:49 -0400

    app ready

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,40 +1,39 @@
 import React, { Component } from 'react';
-import FlatList from './flat_list';
 import flats from '../../data/flats';
-import googleMapReact from 'google-map-react';
+import FlatList from './flat_list';
+import Marker from './marker';
+import GoogleMapReact from 'google-map-react';
 
 class App extends Component {
   constructor(props) {
     super(props);
-
     this.state = {
-      selectedFlat: flats[0],
-      flats
+      flats,
+      selectedFlat: flats[0]
+    };
+  }
+
+  center() {
+    return {
+      lat: this.state.selectedFlat.lat,
+      lng: this.state.selectedFlat.lng
     };
   }
 
   selectFlat = (index) => {
     this.setState({ selectedFlat: flats[index] });
   }
-
   render() {
-    return (
+    return(
       <div>
-        <FlatList
-          flats={this.state.flats} selectFlat={this.selectFlat}/>
-      </div>
+        <FlatList flats={this.state.flats} selectFlat={this.selectFlat}/>
       <div className='map-container'>
-        <GoogleMapReact
-          bootstrapURLKeys={{ key: "AIzaSyD2TYYUMfsDSaEY576cRd1dUd0HSMhnq98" }}
-        >
-          <AnyReactComponent
-            lat={this.state.selectedFlat.lat}
-            lng={this.state.selectedFlat.lng}
-            text="My Marker"
-          />
+        <GoogleMapReact defaultCenter={this.center()} defaultZoom={12} >
+          <Marker lat={this.state.selectedFlat.lat} lng={this.state.selectedFlat.lng} />
         </GoogleMapReact>
       </div>
-    );
+    </div>
+    )
   }
 }
 
--- src/components/flat_list.jsx
+++ src/components/flat_list.jsx
@@ -2,17 +2,17 @@ import React from 'react';
 import Flat from './flat';
 
 const FlatList = (props) => {
+
   const renderList = () => {
     return props.flats.map((flat, index) => {
-      return <Flat flat={flat} index={index} selectFlat={props.selectFlat} />
+      return <Flat flat={flat} index={index} selectFlat={props.selectFlat} />;
     });
-  };
-
+  }
   return (
-    <div className="flat-list">
-      {renderList()}
+    <div className='flat-list'>
+    {renderList()}
     </div>
-  );
-};
+  )
+}
 
-export default FlatList;
+export default FlatList
